403022f2e136c3f209d3316c7517c47decdd6cfb
Benji Martinez Flores <49083076+Benji-Mtz@users.noreply.github.com>
2022-02-03 19:48:54 -0600

    comment in global module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,6 +3,7 @@ import { Module, Global } from '@nestjs/common';
 const API_KEY = '123456';
 const API_KEY_PROD = 'ValPROD';
 
+// Global dice que seran providers globales
 @Global()
 @Module({
   providers: [
@@ -11,6 +12,7 @@ const API_KEY_PROD = 'ValPROD';
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
   ],
+  // hacemos que el API_KEY sea utilizado desde cualquier modulo
   exports: ['API_KEY'],
 })
 export class DatabaseModule {}
